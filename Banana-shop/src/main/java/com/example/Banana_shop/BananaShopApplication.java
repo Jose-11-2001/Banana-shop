61f44929eea5687c46866e16ec2e708ebaef0e8d
Jose-11-2001 <josephmbukwa2001@gmail.com>
2026-06-09 22:44:26 +0200

    first commit

---
+++ Banana-shop/src/main/java/com/example/Banana_shop/BananaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Banana_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananaShopApplication.class, args);
+	}
+
+}
---
+++ Banana-shop/src/test/java/com/example/Banana_shop/BananaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Banana_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
